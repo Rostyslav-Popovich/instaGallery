dc781998a93fcd0b38b7d3eb857e25c9e8e5a052
Rostyslav Popovich <p.rostyslav@improveit.solutions>
2021-04-07 12:26:17 +0300

    created GalleryDiffUtil

---
+++ app/src/main/java/com/example/myapplication/ui/gallery/view/adapter/GalleryDiffUtil.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication.ui.gallery.view.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.myapplication.data.model.Data
+
+class GalleryDiffUtil(private val oldData: List<Data>, private val newData: List<Data>) : DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int {
+        return oldData.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newData.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldData[oldItemPosition]
+        val newItem = newData[newItemPosition]
+        return oldItem.id==newItem.id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldData[oldItemPosition]
+        val newItem = newData[newItemPosition]
+        return oldItem.media_url.equals(newItem.media_url)
+    }
+}
